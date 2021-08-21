c52406865c87f282ee3daffbbf7b5c44863ce51f
Kamrun Nahar <knahar.kamrun@gmail.com>
2021-08-21 11:04:01 +0600

    Refactorized & working

--- js/app.js
+++ js/app.js
@@ -1,86 +1,62 @@
+// -------- Returning Property and respective price and assiging to the relevant ID --------------
+
+function costPrice(property, price) {
+  document.getElementById("extra" + property + "Cost").innerText = price;
+  tableTotalCalculation();
+}
+
+// ------------------ Total Calculation -------------
+
 function tableTotalCalculation() {
   var tableTotal =
     1299 +
     parseInt(document.getElementById("extraMemoryCost").innerText) +
     parseInt(document.getElementById("extraStorageCost").innerText) +
     parseInt(document.getElementById("extraDeliveryCost").innerText);
-
-  document.getElementById("table-total").innerText = tableTotal;
-  document.getElementById("grand-total").innerText = tableTotal;
+  document.getElementById("grand-total").innerText = document.getElementById(
+    "table-total"
+  ).innerText = tableTotal;
 }
 
-function memoryCostPrice(ram) {
-  if (ram == 8) {
-    document.getElementById("extraMemoryCost").innerText = 0;
-  }
-  if (ram == 16) {
-    document.getElementById("extraMemoryCost").innerText = 180;
-  }
-}
-
-document.getElementById("memory8").addEventListener("click", function () {
-  memoryCostPrice(8);
-  tableTotalCalculation();
-});
-
-document.getElementById("memory16").addEventListener("click", function () {
-  memoryCostPrice(16);
-  tableTotalCalculation();
-});
-
-// -------------------------------
-function storageCostPrice(storage) {
-  if (storage == 256) {
-    document.getElementById("extraStorageCost").innerText = 0;
-  }
-  if (storage == 512) {
-    document.getElementById("extraStorageCost").innerText = 100;
-  }
-  if (storage == 1) {
-    document.getElementById("extraStorageCost").innerText = 180;
-  }
-}
-
-document.getElementById("storage256").addEventListener("click", function () {
-  storageCostPrice(256);
-  tableTotalCalculation();
-});
-document.getElementById("storage512").addEventListener("click", function () {
-  storageCostPrice(512);
-  tableTotalCalculation();
-});
-document.getElementById("storage1t").addEventListener("click", function () {
-  storageCostPrice(1);
-  tableTotalCalculation();
-});
-
-// -------------------------------
-function deliveryCostPrice(deliveryType) {
-  if (deliveryType == "free") {
-    document.getElementById("extraDeliveryCost").innerText = 0;
-  }
-  if (deliveryType == "quick") {
-    document.getElementById("extraDeliveryCost").innerText = 20;
-  }
-}
-
-document.getElementById("deliveryFree").addEventListener("click", function () {
-  deliveryCostPrice("free");
-  tableTotalCalculation();
-});
-
-document.getElementById("deliveryQuick").addEventListener("click", function () {
-  deliveryCostPrice("quick");
-  tableTotalCalculation();
-});
-
-// ------------------ Coupon Apply -------------
+// ------------------ Coupon Apply & Grand Total Calculation -------------
 
 function couponApplied() {
-  if (document.getElementById("promoCodeInput").value == "stevekaku") {
+  if (
+    document.getElementById("promoCodeInput").value.toLowerCase() == "stevekaku"
+  ) {
     document.getElementById("grand-total").innerText =
       parseInt(document.getElementById("table-total").innerText) * 0.8;
   }
-  var emptyString = "";
-  document.getElementById("promoCodeInput").value = emptyString;
+  document.getElementById("promoCodeInput").value = "";
 }
+
+// --------------------All button Event Listener ---------------------
+
+document.getElementById("memory8").addEventListener("click", function () {
+  costPrice("Memory", 0);
+});
+
+document.getElementById("memory16").addEventListener("click", function () {
+  costPrice("Memory", 180);
+});
+
+// ---------------------- Storage Buttons  ----------------------
+document.getElementById("storage256").addEventListener("click", function () {
+  costPrice("Storage", 0);
+});
+document.getElementById("storage512").addEventListener("click", function () {
+  costPrice("Storage", 100);
+});
+document.getElementById("storage1t").addEventListener("click", function () {
+  costPrice("Storage", 180);
+});
+
+// ----------------------Delivery Buttons  ----------------------
+
+document.getElementById("deliveryFree").addEventListener("click", function () {
+  costPrice("Delivery", 0);
+});
+
+document.getElementById("deliveryQuick").addEventListener("click", function () {
+  costPrice("Delivery", 20);
+});
